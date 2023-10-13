8406dd903cd81e5470237865f49767dd86eab14c
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-14 01:09:24 +0530

    Added custom error functionality

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,3 +1,8 @@
+const customError = require("../errors/custom-errors")
+
 module.exports= (err,req,res,next)=>{
+    if(err instanceof customError){
+        return res.status(err.statusCode).json({msg:err.message})
+    }
     return res.status(500).json({msg:"Something went wrong"})
 }
